4fa6e66b7eebdc8f7794f0f28ea4f75e1645f909
juliamalin <juliamalin.jm@gmail.com>
2023-06-14 16:52:46 +0300

    käyttäjien hallinta ja Token-perustainen kirjautuminen

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,23 +1,46 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
 
     blogsRouter.get('/', async (request, response) => {
-      const blogs = await Blog.find({})
+      const blogs = await Blog
+      .find({}).populate('user',{username: 1, name: 1})
+      
       response.json(blogs)
     })
 
+    const getTokenFrom = request => {
+      const authorization = request.get('authorization')
+      if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+      }
+      return null
+    }
+
     blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
+
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes || 0
+        likes: body.likes || 0,
+        user: user._id
     })
 
     try {
       const savedBlog = await blog.save()
+      user.blogs = user.blogs.concat(savedBlog._id)
+      await user.save()
       response.status(201).json(savedBlog)
     } catch(exception) {
       next(exception)
@@ -39,12 +62,27 @@ const Blog = require('../models/blog')
   })
 
   blogsRouter.delete('/:id', async (request, response, next) => {
+
     try {
+      const decodedToken = jwt.verify(request.token, process.env.SECRET)
+      if(!decodedToken.id) {
+        return response.status(401).json({error: 'Token invalid'})
+      }
+      
+      const blog = await Blog.findById(request.params.id)
+      if (!blog) {
+        return response.status(404).json({ error: 'Blog not found' })
+      }
+
+      if (blog.user.toString() !== decodedToken.id) {
+        return response.status(403).json({ error: 'Unauthorized' })
+      }
+
       await Blog.findByIdAndRemove(request.params.id)
       response.status(204).end()
-    } catch (exception) {
-      next(exception)
-    }
-  })
+      } catch (exception) {
+        next(exception)
+      }
+    })
 
 module.exports = blogsRouter
--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 const supertest = require('supertest')
+const bcrypt = require('bcrypt')
+
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const helper = require('./test_helper')
   
 beforeEach(async () => {
@@ -130,6 +134,125 @@ test('a blog can be deleted', async () => {
   const contents = blogsAtEnd.map(r => r.title)
 
   expect(contents).not.toContain(blogToDelete.title)
+})
+
+
+test('a blog can be changed ', async () => {
+  const updateBlog = {
+    title: 'lillin blogi',
+    author: 'lilli gunnar',
+    url:'lilli.gunnar@blogi.fi',
+    likes: 200,
+  }
+
+  await api
+    .put('/api/blogs')
+    .send(updateBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+
+  const titles = blogsAtEnd.map(b => b.title)
+  expect(titles).toContain(
+    'lillin blogi'
+  )
+})
+
+describe('when there is initially one user at db', ()=> {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async() => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'jmalin',
+      name: 'Julia Malin',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length +1 )
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async() => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen'
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+    
+    expect(result.body.error).toContain('expected `username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
+
+  test('creation fails if username is too short', async() => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'ro',
+      name: 'RO',
+      password: 'sala'
+    }
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+    
+    expect(result.body.error).toContain("User validation failed: username: Path `username` (`ro`) is shorter than the minimum allowed length (3).")
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+  })
+
+  test('creation fails if there is no username', async() => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: '',
+      name: 'keke',
+      password: 'sala'
+    }
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+    
+    expect(result.body.error).toContain( "User validation failed: username: Path `username` is required.")
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+  })
 })
 
 
