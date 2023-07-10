b7ede38777c793288be080bc60cba7406816c1d6
juliamalin <juliamalin.jm@gmail.com>
2023-07-10 14:24:36 +0300

    redux Thunk -kirjaston käyttö palvelimen kanssa kommunikointiin

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,12 +1,17 @@
 import AnecdoteForm from './components/Anecdoteform'
 import AnecdoteList from './components/Anecdotelist'
-
-
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 
 const App = () => {
-
-
+  const dispatch = useDispatch()
   
+  useEffect(() => {
+    dispatch(initializeAnecdotes())
+  }, [dispatch])
+
+
   return (
     <div>
       <h2>Anecdotes</h2>
